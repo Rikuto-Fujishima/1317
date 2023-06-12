0ef50851a29a356aa8ee0fa05ace13fe26c54ae5
Rikuto-Fujishima <127046429+Rikuto-Fujishima@users.noreply.github.com>
2023-06-12 18:59:36 +0800

    Add files via upload

---
+++ Assignment4/Alm/Alarm.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+namespace clock
+{
+    class Time
+    {
+        private int h;
+        private int m;
+        private int s;
+        public int Hour => h;
+        public int Minute => m;
+        public int Second => s;
+        public Time(int h = 0, int m = 0, int s = 0)
+        {
+            this.h = h;
+            this.m = m;
+            this.s = s;
+        }
+        public bool valid()
+        {
+            return h > 0 && m >= 0 && s >= 0
+                && h < 24 && m < 60 && s < 60;
+        }
+        public override bool Equals(object obj)
+        {
+            var time = obj as Time;
+            return time != null &&
+                   Hour == time.Hour &&
+                   Minute == time.Minute &&
+                   Second == time.Second;
+        }
+        public override int GetHashCode()
+        {
+            var hashCode = 1505761165;
+            hashCode = hashCode * -1521134295 + Hour.GetHashCode();
+            hashCode = hashCode * -1521134295 + Minute.GetHashCode();
+            hashCode = hashCode * -1521134295 + Second.GetHashCode();
+            return hashCode;
+        }
+        public override string ToString()
+        {
+            return Hour + ":" + Minute + ":" + Second;
+        }
+    }
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                AlarmClock clock = new AlarmClock(); 
+                clock.AlarmTime = new Time(DateTime.Now.Hour,
+                              DateTime.Now.Minute,
+                              DateTime.Now.Second + 5);
+                int total = 0;
+                clock.TickEvent += (c => Count(c, ref total));
+                new Thread(clock.Run).Start();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            static void Count(AlarmClock sender, ref int total)
+            {
+                Time time = sender.CurrentTime;
+                total = total + 1;
+                Console.WriteLine($"Total= {total}");
+            }
+            static void PlayMusic(AlarmClock sender)
+            {
+                Time time = sender.CurrentTime;
+                Console.WriteLine($"Event: {time.Hour}:{time.Minute}:{time.Second}");
+                Console.WriteLine("Playing.");
+            }
+        }
+        class AlarmClock
+        {
+            public event Action<AlarmClock> AlarmEvent;
+            public event Action<AlarmClock> TickEvent;
+            public AlarmClock()
+            {
+                CurrentTime = new Time();
+                TickEvent += c => Console.WriteLine("Tick!" + CurrentTime);
+                AlarmEvent += c => Console.WriteLine("Alarm!");
+            }
+            public Time CurrentTime { get; set; }
+            public Time AlarmTime { get; set; }
+            public void Run()
+            {
+                while (true)
+                {
+                    DateTime now = DateTime.Now;
+                    CurrentTime = new Time(now.Hour, now.Minute, now.Second);
+                    TickEvent(this);
+                    if (AlarmTime.Equals(CurrentTime))
+                        AlarmEvent(this);
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
---
+++ Assignment4/Lklst/Linklist.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace GenericApplication
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> next;
+        public GenericList()
+        {
+            next = head = null;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (next == null)
+            {
+                head = next = n;
+            }
+            else
+            {
+                next.Next = n;
+                next = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            for (Node<T> n = head; n != null; n = n.Next)
+            {
+                action(n.Data);
+            }
+        }
+        public double Sum(Func<T, double> selector)
+        {
+            double sum = 0;
+            for (Node<T> n = head; n != null; n = n.Next)
+            {
+                sum += selector(n.Data);
+            }
+            return sum;
+        }
+    }
+    class Lklist
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            GenericList<int> lsti = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                lsti.Add(random.Next(1000));
+            }
+            lsti.ForEach(n => Console.Write(n + "\t"));
+            double min = double.MaxValue;
+            double max = double.MinValue;
+            double sum = 0;
+            lsti.ForEach(n => {
+                min = (n < min) ? n : min;
+                max = (n > max) ? n : max;
+                sum += n;
+            });
+            Console.WriteLine();
+            Console.WriteLine($"min={min},max={max},sum={sum}");
+            sum = lsti.Sum(n => n);
+            Console.WriteLine("sum=" + sum);
+            GenericList<String> lsts = new GenericList<String>();
+            lsts.Add("a");
+            lsts.Add("123");
+            int len = (int)lsts.Sum(s => s.Length);
+            Console.WriteLine("len=" + len);
+        }
+    }
+}
